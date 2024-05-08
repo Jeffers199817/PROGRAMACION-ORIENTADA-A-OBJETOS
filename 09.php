d7815b9bcb1362fafe95c2e6fb6e09910d0b42ce
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-05-07 23:18:22 -0500

    Final de programación orientada a objetos

--- 09.php
+++ 09.php
@@ -5,6 +5,44 @@
 //Conectar a la bd con Mysqli.
 
 $db = new mysqli('localhost','root','Jeff1998','bienesraices_crud');
-var_dump($db);
+
+//CREAMOS EL QUERY
+$query = 'SELECT titulo, imagen FROM propiedades';
+
+  //3. LO PREPARAMOS
+$stmt = $db->prepare($query);
+
+//4.LO EJECUTAMOS 
+
+$stmt -> execute();
+
+//5. CREAMOS UNA VARIABLE
+
+$stmt->bind_result($titulo, $imagen);
+/* 
+// ASIGNAMOS EL RESULTADO
+$stmt->fetch();
+
+//imprimir el resultado 
+
+var_dump($titulo);
+var_dump( $imagen);
+ */
+//PARA IMPRIMIR EL RESULTADO A MULTIPLES 
+
+while ($stmt->fetch()):
+    var_dump($titulo);
+    endwhile;
+
+
+//UTILIZAMOS ESTE CODIGO PAR PODER ITERAR TODO LO QUE TIENE EN LA BASE DE DATOS
+
+/* while($row = $resultado->fetch_assoc()):
+    var_dump($row);
+endwhile; */
+
+// UTILIZAMOS ESTE CODIGO PARA REALIZAR UNA SOLA ITERACIÓN. 
+
+/* var_dump($resultado->fetch_assoc()); */
 
 include 'includes/footer.php';
--- 10.php
+++ 10.php
@@ -1,4 +1,43 @@
-<?php include 'includes/header.php';
+<?php 
 
+
+include 'includes/header.php';
+
+
+//CONECTAR A LA BD CON PDO
+
+$db = new PDO('mysql:host=localhost; dbname=bienesraices_crud','root','Jeff1998');
+
+//Creamos el query
+
+$query = "SELECT titulo FROM propiedades";
+
+
+//Lo preparamos
+
+$stmt = $db->prepare($query);
+
+
+//Lo ejecutamos 
+$stmt->execute();
+
+
+//obtener resultados 
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+foreach($result as $propiedad):
+    echo $propiedad['titulo'];
+    echo '<br>';
+    echo $propiedad['imagen'];
+    echo '<br>';
+    endforeach;
+echo "<pre>";
+var_dump($result);
+echo "</pre>";
 
 include 'includes/footer.php';
+
+?>
